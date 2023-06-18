c87a59ee32c4a81e4c092005937cdead3e02dbc1
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-06-17 21:41:15 -0500

    Integración de la plantilla AdminLTE con Thymeleaf y Springboot, se agreo el layout, index y login

---
+++ db-creator/infrastructure/entry-points/api-rest/src/main/java/co/com/siigroup/api/Show.java
@@ -0,0 +1,23 @@
+
+package co.com.siigroup.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Show {
+    @RequestMapping("/layout")
+    public String dashboard(){
+        return "layout";
+    }
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
